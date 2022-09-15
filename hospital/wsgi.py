64d0b69d4c62d37d5aac5f05061d2bc6beb83e18
Loicsurf <58171734+Loicsurf@users.noreply.github.com>
2022-09-15 11:04:55 +0100

    Update wsgi.py

--- hospital/wsgi.py
+++ hospital/wsgi.py
@@ -12,7 +12,7 @@ from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospital.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospital.settings.prod')
 
 application = get_wsgi_application()
 application = WhiteNoise(application)
